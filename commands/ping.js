ad6cba9fe07c69052a241123062d9eabce91130a
ronkili <ronaa0269@gmail.com>
2026-04-29 20:10:45 +0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,11 @@
+const { SlashCommandBuilder } = require('discord.js');
+
 module.exports = {
-    name: "ping",
-    execute(message) {
-        message.reply("Pong!");
-    }
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Replies with Pong!'),
+
+    async execute(interaction) {
+        await interaction.reply('Pong!');
+    },
 };
