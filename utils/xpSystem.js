af7a5470cb37e359ebc74cec66c88e98184827e8
ronkili <ronaa0269@gmail.com>
2026-05-08 12:31:52 +0300

    Update xpSystem.js

--- utils/xpSystem.js
+++ utils/xpSystem.js
@@ -3,27 +3,6 @@ const path = require('path');
 
 const dbPath = path.join(__dirname, '..', 'xpData.json');
 
-const shopItems = [
-    {
-        id: 'vip',
-        name: 'VIP Role',
-        price: 500,
-        roleId: 'PUT_VIP_ROLE_ID'
-    },
-    {
-        id: 'color',
-        name: 'Special Color Role',
-        price: 300,
-        roleId: 'PUT_COLOR_ROLE_ID'
-    },
-    {
-        id: 'legend',
-        name: 'Legend Role',
-        price: 1000,
-        roleId: 'PUT_LEGEND_ROLE_ID'
-    }
-];
-
 function loadData() {
     if (!fs.existsSync(dbPath)) {
         fs.writeFileSync(dbPath, JSON.stringify({}, null, 4));
@@ -43,7 +22,7 @@ function getUserData(userId) {
         data[userId] = {
             xp: 0,
             level: 1,
-            coins: 0
+            totalXp: 0
         };
 
         saveData(data);
@@ -59,21 +38,19 @@ function addXp(userId, amount) {
         data[userId] = {
             xp: 0,
             level: 1,
-            coins: 0
+            totalXp: 0
         };
     }
 
     data[userId].xp += amount;
-    data[userId].coins += amount;
+    data[userId].totalXp += amount;
 
-    const neededXp = data[userId].level * 100;
     let leveledUp = false;
 
-    while (data[userId].xp >= neededXp) {
-        data[userId].xp -= neededXp;
+    while (data[userId].xp >= data[userId].level * 100) {
+        data[userId].xp -= data[userId].level * 100;
         data[userId].level += 1;
         leveledUp = true;
-        break;
     }
 
     saveData(data);
@@ -84,39 +61,18 @@ function addXp(userId, amount) {
     };
 }
 
-function removeCoins(userId, amount) {
-    const data = loadData();
-
-    if (!data[userId]) {
-        data[userId] = {
-            xp: 0,
-            level: 1,
-            coins: 0
-        };
-    }
-
-    if (data[userId].coins < amount) {
-        return false;
-    }
-
-    data[userId].coins -= amount;
-    saveData(data);
-
-    return true;
-}
-
 function getLeaderboard() {
     const data = loadData();
 
     return Object.entries(data)
-        .sort((a, b) => b[1].level - a[1].level || b[1].xp - a[1].xp)
+        .sort((a, b) => {
+            return b[1].level - a[1].level || b[1].xp - a[1].xp;
+        })
         .slice(0, 10);
 }
 
 module.exports = {
-    shopItems,
     getUserData,
     addXp,
-    removeCoins,
     getLeaderboard
 };
