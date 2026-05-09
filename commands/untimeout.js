c99f3d35b9a8edb9e0f187f8dbf14d54ee1dad8a
ronkili <ronaa0269@gmail.com>
2026-05-09 13:20:28 +0300

    Update untimeout.js

--- commands/untimeout.js
+++ commands/untimeout.js
@@ -26,8 +26,8 @@ module.exports = {
 
         try {
 
-            const staffRoleId = "1496162203147964426";
-            const logsChannelId = "1497959735607951430";
+            const staffRoleId = "1502608350364565695";
+            const logsChannelId = "1502608488939917362";
 
             if (!interaction.member.roles.cache.has(staffRoleId)) {
                 return interaction.editReply("❌ אין לך הרשאה להשתמש בפקודה הזאת");
