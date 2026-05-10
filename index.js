f82976f953124eb295cf74b0f09c9efbf556449a
ronkili <ronaa0269@gmail.com>
2026-05-10 17:02:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const {
     ButtonStyle,
     Collection,
     ChannelType,
-    PermissionFlagsBits
+    PermissionFlagsBits,
+    ActivityType
 } = require('discord.js');
 
 const fs = require('fs');
