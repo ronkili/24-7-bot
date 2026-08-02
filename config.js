ff9c401c4282d9d6db2ee65632ca14e159bc4dc7
ronkili <ronaa0269@gmail.com>
2026-08-02 21:36:41 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,9 +4,15 @@ module.exports = {
 
   memberRoleId: "1513657198297747526",
   securityLogChannelId: "1514983414254538903",
+
   ticketCategoryId: "1513657208557146142",
-ticketStaffRoleId: "1523770519285923860",
-ticketLogsChannelId: "1514982641063952394",
+  ticketStaffRoleId: "1523770519285923860",
+  ticketLogsChannelId: "1514982641063952394",
+
+  vipRoleId: "1513657194472669294",
+  staffRoleId: "1514982124808048765",
+  ownerRoleId: "1513657191465353296",
+  vipRequestsChannelId: "1533544032226115715",
 
   allowedBotIds: [
     "1514934539091181798"
